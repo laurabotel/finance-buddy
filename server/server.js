2a48068b8968e6f19091d5359c679689f4f93858
laurabotel <laura.e.botel@gmail.com>
2023-05-27 19:59:03 -0400

    adds global error handler

--- server/server.js
+++ server/server.js
@@ -21,9 +21,14 @@ app.use('/', (req, res) => {
 });
 
 app.use((err, req, res, next) => {
-  console.log(err);
-
-  return res.status(500).send('Error in server');
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
 });
 
 app.listen(4000, () => {
